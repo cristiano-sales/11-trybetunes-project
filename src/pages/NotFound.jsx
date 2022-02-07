8b4ca75d8e77976c991b09305b0065db5513fcfd
Cristiano Sales <cristianosalesneves@gmail.com>
2022-02-07 15:08:32 -0300

    1 - Crie as rotas necessárias para a aplicação

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <p>componente NotFound</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
