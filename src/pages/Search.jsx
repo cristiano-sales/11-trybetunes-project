baee13c8f825427507cc1bae24127ccf00901f4d
Cristiano Sales <cristianosalesneves@gmail.com>
2022-02-08 11:57:44 -0300

    05.SearchPage.test

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,47 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state ={
+    textoDigitado: '',
+    botaoPesquisarDesabilitado: true,
+  }
+
+  handleInputChange = ({ target }) => {
+    this.setState({ textoDigitado: target.value }, this.verificaCaracteresBotao);
+  }
+
+  verificaCaracteresBotao = () => {
+    const minimoCaracteres = 2;
+    const { textoDigitado } = this.state;
+    if (textoDigitado.length >= minimoCaracteres) {
+      this.setState({ botaoPesquisarDesabilitado: false });
+    } else {
+      this.setState({ botaoPesquisarDesabilitado: true });
+    }
+  }
+
   render() {
+    const { botaoPesquisarDesabilitado } = this.state;
     return (
       <div>
+        <h1>COMPONENTE SEARCH</h1>
         <Header />
         <div data-testid="page-search">
-          <p>componente Search</p>
+          <form>
+            <input
+              data-testid="search-artist-input"
+              onChange={ this.handleInputChange }
+              type="text"
+              placeholder="Banda ou Artista"
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ botaoPesquisarDesabilitado }
+              type="button"
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </div>
     );
